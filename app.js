5a0506f3dc7ac72d29175aab452aab5751a69733
Abigail McFarlane <abigail.mcfarlane@mymona.uwi.edu>
2022-11-05 20:57:05 -0500

    Exercise 3 and chsnges to html, css and js files

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 window.onload = function(){
-    let searchBtn = document.querySelector("#btn")
+    let searchBtn = document.querySelector("#btn");
+    let result = document.querySelector("#result");
+    // let input = document.querySelector("input");
+    // let query = input.value ? input.value : "";
 
     searchBtn.addEventListener("click", (event)=>{
-        fetch("http://localhost/info2180-lab4/superheroes.php")
+        let input = document.querySelector("#NameOrAlies");
+        let query = input.value ? input.value : "";
+        fetch(`http://localhost/info2180-lab4/superheroes.php?query=${query.trim()}`)
         .then(response => {
             return response.text();
         })
         .then(data => {
-            alert(data)
+            result.innerHTML = data;
+        })
+        .catch(err => {
+            console.log(err);
         })
     })
 }
